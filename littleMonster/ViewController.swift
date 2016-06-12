251eb49be63a404d9f08ae359fdcdecc3a4380dd
Khaled Rahman Ayon <khaledrahmanayon@khaleds-air.fritz.box>
2016-06-12 22:11:53 +0200

    Initial Commit

---
+++ littleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  littleMonster
+//
+//  Created by Khaled Rahman Ayon on 12/06/16.
+//  Copyright © 2016 iosApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
